5b0c86429c7505581fa5d8107a7bd404ba56a8a6
Priyansh Shah <Priyanshpokemon@gmail.com>
2023-11-14 10:16:23 -0800

    Starting with React
    First Commit to AcademiX

---
+++ academix/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AcademicPlanner from './components/AcademicPlanner'; // Make sure the path is correct
+
+
+function App() {
+  return (
+    <div className="App">
+      <AcademicPlanner />
+    </div>
+  );
+}
+
+export default App;
